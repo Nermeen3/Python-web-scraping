fe048671d61644224f61c75c60e075c894bf3b19
Nermeen <63965785+Nermeen3@users.noreply.github.com>
2022-12-15 16:34:26 +0400

    Update notification_manager.py

--- Cheap Flight Club/notification_manager.py
+++ Cheap Flight Club/notification_manager.py
@@ -3,16 +3,16 @@ from twilio.rest import Client
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.account_sid = "AC8d94d30af05d44101787ca272e41de56"
-        self.auth_token = "416c08a8fd0b9991f2749587b890a9e0"
+        self.account_sid = "ABC"
+        self.auth_token = "ABC"
         self.client = Client(self.account_sid, self.auth_token)
 
     def send_sms(self):
         message_body = f"Low Price Alert!\n"
         message = self.client.messages.create(
             body=message_body,
-            from_='+19283707954',
-            to='+995558871331'  # 995593319561
+            from_='+11111111111',
+            to='+22222222222'
         )
         print(message.sid)
 
